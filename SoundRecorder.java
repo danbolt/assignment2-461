00b88ac0ca0b4ef09311b743760ae32a599a1bf8
Daniel Savage <dansvg@gmail.com>
2013-01-25 14:55:09 -0800

    added a quick top-level menu

--- SoundRecorder.java
+++ SoundRecorder.java
@@ -2,6 +2,7 @@ import javax.media.*;
 
 import javax.swing.*;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,9 +11,31 @@ import java.net.MalformedURLException;
 
 public class SoundRecorder extends JPanel
 {
-	public SoundRecorder()
+	//GUI Component classes
+	JMenuBar topMenuBar;
+
+	public SoundRecorder(JMenuBar newBar)
 	{
-		//
+		topMenuBar = newBar;
+
+		//add menus to topMenuBar
+		{
+			JMenu fileMenu = new JMenu("File");
+			fileMenu.setMnemonic(KeyEvent.VK_F);
+			topMenuBar.add(fileMenu);
+			
+			JMenu editMenu = new JMenu("Edit");
+			editMenu.setMnemonic(KeyEvent.VK_E);
+			topMenuBar.add(editMenu);
+			
+			JMenu optionsMenu = new JMenu("Options");
+			optionsMenu.setMnemonic(KeyEvent.VK_O);
+			topMenuBar.add(optionsMenu);
+			
+			JMenu helpMenu = new JMenu("Help");
+			helpMenu.setMnemonic(KeyEvent.VK_H);
+			topMenuBar.add(helpMenu);
+		}
 	} //constructor
 	
 	public void paint(Graphics g)
@@ -22,11 +45,16 @@ public class SoundRecorder extends JPanel
 	
 	public static void main(String[] args)
 	{
+		JMenuBar bar = new JMenuBar();
 		JFrame applicationFrame = new JFrame();
-		applicationFrame.getContentPane().add(new SoundRecorder());
+		applicationFrame.getContentPane().add(new SoundRecorder(bar));
 		
 		applicationFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		applicationFrame.setJMenuBar(bar);
 		applicationFrame.setSize(300, 200);
+		applicationFrame.setTitle("Sound Tool");
+		applicationFrame.setResizable(false);
+		applicationFrame.setLocation(300, 300);
 		applicationFrame.setVisible(true);
 	} //main
 
