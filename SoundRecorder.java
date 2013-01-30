2f5362ee7a612af3f784983e72e646e99d712076
Daniel Savage <dansvg@gmail.com>
2013-01-30 01:31:05 -0800

    play/pause buttons now play/pause loaded media

--- SoundRecorder.java
+++ SoundRecorder.java
@@ -3,14 +3,17 @@ import javax.media.*;
 import javax.swing.*;
 import java.awt.FileDialog;
 import java.awt.Graphics;
+import java.awt.BorderLayout;
 import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.net.MalformedURLException;
 
-public class SoundRecorder extends JPanel
+public class SoundRecorder extends JPanel implements ActionListener
 {
 	/* JMF classes  */
 	private Player audioPlayer = null;
@@ -34,6 +37,9 @@ public class SoundRecorder extends JPanel
 			JMenu fileMenu = new JMenu("File");
 			fileMenu.setMnemonic(KeyEvent.VK_F);
 			JMenuItem openFile = new JMenuItem("Open Media...");
+			openFile.setActionCommand("openFile");
+			openFile.addActionListener(this);
+			fileMenu.add(openFile);
 			topMenuBar.add(fileMenu);
 
 			JMenu editMenu = new JMenu("Edit");
@@ -43,21 +49,75 @@ public class SoundRecorder extends JPanel
 			JMenu optionsMenu = new JMenu("Options");
 			optionsMenu.setMnemonic(KeyEvent.VK_O);
 			topMenuBar.add(optionsMenu);
-			
+
 			JMenu helpMenu = new JMenu("Help");
 			helpMenu.setMnemonic(KeyEvent.VK_H);
 			topMenuBar.add(helpMenu);
 		}
+		
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+		JSlider audioSlider = new JSlider(0, 1000, 0);
+		add(audioSlider);
+		
+		JButton button;
+		button = new JButton("Play");
+		button.setActionCommand("playSong");
+		button.addActionListener(this);
+		add(button);
+                button = new JButton("Stop");
+                button.setActionCommand("stopSong");
+                button.addActionListener(this);
+		add(button);
+		button = new JButton("Forward");
+		button.setActionCommand("fastForward");
+		button.addActionListener(this);
+		add(button);
+		button = new JButton("Rewind");
+		button.setActionCommand("rewind");
+		button.addActionListener(this);
+		add(button);
+		button = new JButton("Record");
+		button.setActionCommand("startRecording");
+		button.addActionListener(this);
+		add(button);
 	} //constructor
-	
+
+	public void actionPerformed(ActionEvent ex)
+	{
+		try
+		{
+			if ("openFile".equals(ex.getActionCommand()))
+			{
+				openAudioFile();
+			}
+			else if ("playSong".equals(ex.getActionCommand()))
+			{
+				audioPlayer.start();
+			}
+			else if ("stopSong".equals(ex.getActionCommand()))
+			{
+				audioPlayer.stop();
+			}
+			else
+			{
+				System.out.println("'" + ex.getActionCommand() + "' has not been initalized yet");
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	} //actionPerformed
+
 	private void openAudioFile() throws java.io.IOException, java.net.MalformedURLException, javax.media.MediaException
 	{
-		FileDialog fd = new FileDialog ((JFrame)getParent(), "Open media file...", FileDialog.LOAD);
+		FileDialog fd = new FileDialog ((JFrame)(SwingUtilities.getWindowAncestor(this)), "Open media file...", FileDialog.LOAD);
 		fd.setVisible(true);
 		File f = new File(fd.getDirectory(), fd.getFile());
 		
 		audioPlayer = Manager.createRealizedPlayer(f.toURI().toURL());
-	}
+	} //openAudioFile
 
 	public static void main(String[] args)
 	{
@@ -67,10 +127,11 @@ public class SoundRecorder extends JPanel
 		
 		applicationFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		applicationFrame.setJMenuBar(bar);
-		applicationFrame.setSize(300, 200);
+		applicationFrame.setSize(350, 200);
 		applicationFrame.setTitle("Sound Tool");
 		applicationFrame.setResizable(false);
 		applicationFrame.setLocation(300, 300);
+		applicationFrame.pack();
 		applicationFrame.setVisible(true);
 	} //main
 
